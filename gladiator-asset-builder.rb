5f9d86fe626eac9b78a3bf39ae986057e1b3cacc
Pavel Kasila <kodosgames@gmail.com>
2020-11-24 22:04:59 +0300

    Update gladiator-asset-builder.rb

--- gladiator-asset-builder.rb
+++ gladiator-asset-builder.rb
@@ -3,7 +3,7 @@ class GladiatorAssetBuilder < Formula
   homepage "https://github.com/GladiatorEngine/gladiator-asset-builder"
   url "https://github.com/GladiatorEngine/gladiator-asset-builder/archive/main.zip"
   version "0.0.1"
-  sha256 "8cb444095c263c2c9b4c5e1156706d10fb42158e58591d6744f09fce28861c54"
+  sha256 "f413695af50d777e49a156cd6076e06e202d0b8a95bd201cc2a7af8690ed5690"
   def install
     system "swift", "build",
         "--configuration", "release",
