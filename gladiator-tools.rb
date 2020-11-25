19cfa63dd503e6fef351666f6667cf59ed80dbbc
Pavel Kasila <kodosgames@gmail.com>
2020-11-25 12:28:16 +0300

    Add SHA256

--- gladiator-tools.rb
+++ gladiator-tools.rb
@@ -3,6 +3,7 @@ class GladiatorTools < Formula
   homepage "https://github.com/GladiatorEngine/gladiator-tools"
   url "https://github.com/GladiatorEngine/gladiator-tools/archive/0.0.1-dev.01.zip"
   version "0.0.1-dev.01"
+  sha256 "31a787120cc13f47039c04fa145203ed4ff9c1c9abfeab8d84af4e6cc859f33d"
   def install
     system "swift", "build",
         "--configuration", "release",
