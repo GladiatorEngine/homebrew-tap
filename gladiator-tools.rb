95adfeb3bf04f36014ee96a7f1e98d4264b2d08f
Pavel Kosilo <kodosgames@gmail.com>
2020-11-26 17:49:48 +0300

    Update gladiator-tools

--- gladiator-tools.rb
+++ gladiator-tools.rb
@@ -1,9 +1,9 @@
 class GladiatorTools < Formula
   desc "GladiatorEngineTools is a toolset which helps developer to work with projects based on GladiatorEngine"
   homepage "https://github.com/GladiatorEngine/gladiator-tools"
-  url "https://github.com/GladiatorEngine/gladiator-tools/archive/0.0.1-dev.01.zip"
-  version "0.0.1-dev.01"
-  sha256 "31a787120cc13f47039c04fa145203ed4ff9c1c9abfeab8d84af4e6cc859f33d"
+  url "https://github.com/GladiatorEngine/gladiator-tools/archive/0.0.1-dev.02.zip"
+  version "0.0.1-dev.02"
+  sha256 "73c8b1e780975fa1fabd2842415f70fd7039032fa37e649d30a15979e4ff941e"
   def install
     system "swift", "build",
         "--configuration", "release",
