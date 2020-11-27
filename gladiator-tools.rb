da75d3e7608960fd7a89805a31037bfb74ef0549
Pavel Kosilo <kodosgames@gmail.com>
2020-11-27 18:55:26 +0300

    Upgrade gladiator-tools to 0.0.1-dev.03

--- gladiator-tools.rb
+++ gladiator-tools.rb
@@ -1,9 +1,9 @@
 class GladiatorTools < Formula
   desc "GladiatorEngineTools is a toolset which helps developer to work with projects based on GladiatorEngine"
   homepage "https://github.com/GladiatorEngine/gladiator-tools"
-  url "https://github.com/GladiatorEngine/gladiator-tools/archive/0.0.1-dev.02.zip"
-  version "0.0.1-dev.02"
-  sha256 "73c8b1e780975fa1fabd2842415f70fd7039032fa37e649d30a15979e4ff941e"
+  url "https://github.com/GladiatorEngine/gladiator-tools/archive/0.0.1-dev.03.zip"
+  version "0.0.1-dev.03"
+  sha256 "b205e1790cb56baa0aaa78d10353adaa5cb5903b7c67138e59ecbc2fe29b2f98"
   def install
     system "swift", "build",
         "--configuration", "release",
